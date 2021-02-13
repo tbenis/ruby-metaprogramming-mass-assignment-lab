862edbeab2bc309d9b8969923bb6c0484274e3f5
Benis Tambe <will.jenifer@yahoo.com>
2021-02-13 17:12:26 -0500

    commit tree c7ce3a2e9e95a96a61c61b4bca16618b4e1fd3a8

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
   #your code here
+  def initialize(attr_methods)
+    attr_methods.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
